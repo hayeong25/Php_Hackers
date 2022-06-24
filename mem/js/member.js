8c98181a21cab10b2f47908994594445bd0783bd
hayeong25 <hayeong0624@gmail.com>
2022-06-24 13:26:12 +0900

    회원유형 값 보내기 추가

--- mem/js/member.js
+++ mem/js/member.js
@@ -5,20 +5,26 @@ $(document).ready(function() {
     $(".lnb").load('/lnb.html');
     $("footer").load('/footer.html');
 
+    var user_type;
+
     // regist_step_01
-    $('.choieAge li').on("click", "a", function(){ // 회원 유형 선택 시
-        var user_type = $("a").attr("id");
-        
-        $.post({
-            type:'post',
-            url:'/member/regist_step_02.html',
-            data:user_type,
-            dataType:'html',
-            success : function(data){
-                console.log(data);
-                $("#content").html('/member/regist_step_02.html div#sub');
+    $('.choiceAge li').on("click", "a", function() { // 회원 유형 선택 시
+        user_type = $(this).attr("id");
+
+        $(this).attr("href", "/member/regist_step_02.html?" + user_type);
+    })
+
+    // regist_step_02
+    $(".btnC").click(function() {
+        $(".checkbox").each(function() {
+            if(":checked") {
+                
+            }else {
+                confirm('모두 동의해야 다음 단계로 넘어갈 수 있습니다.');
+                return;
             }
-        });
+        })
+        $(this).attr("href", "/member/regist_step_03.html?" + user_type);
     })
 })
 
