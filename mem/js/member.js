6efcfeca129d6b655d6a9bae38750eadbe81d934
hayeong25 <hayeong0624@gmail.com>
2022-06-24 13:55:29 +0900

    동의 체크박스 체크여부 추가

--- mem/js/member.js
+++ mem/js/member.js
@@ -5,26 +5,24 @@ $(document).ready(function() {
     $(".lnb").load('/lnb.html');
     $("footer").load('/footer.html');
 
-    var user_type;
+    // 회원유형은 URL에서 ? 뒤에 붙어 따라감
+    var user_type = location.href.split('?')[1];
 
-    // regist_step_01
-    $('.choiceAge li').on("click", "a", function() { // 회원 유형 선택 시
-        user_type = $(this).attr("id");
+    $(function() {
+        // regist_step_01
+        $('.choiceAge li').on("click", "a", function() { // 회원 유형 선택 시
+            user_type = $(this).attr("id");
+    
+            $(this).attr("href", "/member/regist_step_02.html?" + user_type);
+        })
 
-        $(this).attr("href", "/member/regist_step_02.html?" + user_type);
-    })
-
-    // regist_step_02
-    $(".btnC").click(function() {
-        $(".checkbox").each(function() {
-            if(":checked") {
-                
+        // regist_step_02
+        $(".btnC").on("click", "a", function() {
+            if($("[type='checkbox']").length != $("[type='checkbox']:checked").length) {
+                alert('모두 동의해야 다음 단계로 넘어갈 수 있습니다.');
             }else {
-                confirm('모두 동의해야 다음 단계로 넘어갈 수 있습니다.');
-                return;
+                $(this).attr("href", "/member/regist_step_03.html?" + user_type);
             }
         })
-        $(this).attr("href", "/member/regist_step_03.html?" + user_type);
     })
 })
-
