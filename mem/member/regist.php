0f8346a93669a7030d2b1cb72573e0311f34bbd7
hayeong25 <hayeong0624@gmail.com>
2022-06-24 17:44:32 +0900

    PHP MySQL 연동 중

--- mem/member/regist.php
+++ mem/member/regist.php
@@ -16,12 +16,15 @@
     $resut = mysqli_query($con,$query);
 
     if(!$result) {
-        ?>
-        <script> alert('회원가입이 완료되었습니다.'); location.href=".."; </script> 
-        <?php
+?>
+        <script>
+            confirm('회원가입 성공');
+            location.href = "/member/regist_complete.html";
+        </script> 
+<?php
     } else {
-        ?>
+?>
         <script> alert('회원가입에 실패했습니다.\n다시 시도해 주세요.'); location.href=".."; </script>
-        <?php
+<?php
     }
 ?>
